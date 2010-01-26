a1bbf63341887c0984ce0e73548061841e9fd3ef
eykanal@gmail.com <eykanal@gmail.com@20c8cc0c-094a-11df-b996-818f58f34e26>
2010-01-26 05:14:49 +0000

    - removed some complex processing code from .tpl - added todos to processDistrib

--- trunk/processDistrib.php
+++ trunk/processDistrib.php
@@ -7,16 +7,25 @@ if( !$_SESSION['clan_name'] )
 	header( "Location: index.php" );
 
 $clean = array();
-foreach( $_POST as $dirtyLoot->$dirtyDiver )
-{
-	$clean['loot']  = stripslashes( trim( htmlentities( strip_tags( $dirtyLoot  ) ) ) );
-	$clean['diver'] = stripslashes( trim( htmlentities( strip_tags( $dirtyDiver ) ) ) );
 
-	## SET TURNCOUNT TO 0 ##
+foreach( $_POST as $dirtyLoot=>$dirtyDiver )
+{
+	$clean[] = array( 'loot'  => stripslashes( trim( htmlentities( strip_tags( $dirtyLoot  ) ) ) ),
+					  'diver' => stripslashes( trim( htmlentities( strip_tags( $dirtyDiver ) ) ) ),
+				);
 }
 
-## ADD EVERYONE ELSE'S TURNCOUNT TO SAVED TOTAL ##
+$clan = new Clan( CLAN_FILES.'/'.$_SESSION['clan_name'].'/'.$_SESSION['clan_name'].'_actions.txt', CLAN_FILES.'/'.$_SESSION['clan_name'].'/'.$_SESSION['clan_name'].'_divers.txt' );
 
-## SAVE FILE WITH DISTRIB RESULTS ##
+foreach( $clan->divers as $diver )
+{
+	## REDUCE RECIPIENTS TURNCOUNT TO 0 ##
+	
+	## ADD EVERYONE ELSE'S TURNCOUNT TO SAVED TOTAL ##
+	
+	## SAVE FILE WITH DISTRIB RESULTS ##
+	
+	## SAVE FILENAME OF DISTRIB RESULTS TO $_SESSION SO index.php CAN ACCESS AND DISPLAY IT ##
+}
 
 ?>
